a6173cadce60aae390e81d0dc9b4d2534bfdc568
mug <1423mug@gmail.com>
2019-08-11 19:11:43 +0400

    first case

--- wifiman.py
+++ wifiman.py
@@ -32,23 +32,33 @@ class TestWebViewAndroid():
         request.addfinalizer(fin)
         return driver  # provide the fixture value
 
-    def test_add_contacts(self, driver):
-        el = driver.find_element_by_accessibility_id("Add Contact")
+    def test_setup_wifiman(self, driver):
+        # el = driver.find_element_by_accessibility_id("Add Contact")
+        # el.click()
+# кейс 1 установка приложения
+        el = driver.find_element_by_android_uiautomator('new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text("I\'m in").instance(0));')
         el.click()
 
-        textfields = driver.find_elements_by_class_name("android.widget.EditText")
-        textfields[0].send_keys("Appium User")
-        textfields[2].send_keys("someone@appium.io")
+        el = driver.find_element_by_android_uiautomator(
+            'new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text("ALLOW").instance(0));')
+        el.click()
 
-        assert 'Appium User' == textfields[0].text
-        assert 'someone@appium.io' == textfields[2].text
+        # el = driver.find_element_by_android_uiautomator(By.xpath("//*[@text='I'm in']").click())
+        # el.click()
 
-        driver.find_element_by_accessibility_id("Save").click()
+        # textfields = driver.find_elements_by_class_name("android.widget.EditText")
+        # textfields[0].send_keys("Appium User")
+        # textfields[2].send_keys("someone@appium.io")
+
+        # assert 'Appium User' == textfields[0].text
+        # assert 'someone@appium.io' == textfields[2].text
+        #
+        # driver.find_element_by_accessibility_id("Save").click()
 
         # for some reason "save" breaks things
-        alert = driver.switch_to_alert()
+        # alert = driver.switch_to_alert()
 
         # no way to handle alerts in Android
-        driver.find_element_by_android_uiautomator('new UiSelector().clickable(true)').click()
-
-        driver.press_keycode(3)
+        # driver.find_element_by_android_uiautomator('new UiSelector().clickable(true)').click()
+        #
+        # driver.press_keycode(3)
