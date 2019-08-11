87034c59d0f27e951bd3f6789d6f708e4fb0d17e
mug <1423mug@gmail.com>
2019-08-12 00:51:36 +0400

    middle version

--- wifiman.py
+++ wifiman.py
@@ -1,9 +1,10 @@
 import os
 import pytest
+import time
+import numpy
 
 from appium import webdriver
 
-# Returns abs path relative to this file and not cwd
 PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
 )
@@ -30,11 +31,12 @@ class TestWebViewAndroid():
             driver.quit()
 
         request.addfinalizer(fin)
-        return driver  # provide the fixture value
+        return driver
 
     def test_setup_wifiman(self, driver):
 
 # кейс 1 установка приложения
+#TODO убрать явные ожидания
         driver.implicitly_wait(10)
 #"I\'m in"
         el = driver.find_element_by_android_uiautomator('new UiSelector().text("I\'m in")')
@@ -46,20 +48,37 @@ class TestWebViewAndroid():
 
 #открыть заданную сеть
 #"AndroidWifi"
+#TODO Ассерт если сеть не найдена
+#TODO убрать явные ожидания. Заменить на увеличенное значение ожидание элемента
         driver.implicitly_wait(10)
         el = driver.find_element_by_android_uiautomator(
-            # 'new UiScrollable(new UiSelector().instance(0)).scrollIntoView(new UiSelector().text("AndroidWifi").instance(0));')
+            # 'new UiScrollable(new UiSelector().instance(0)).scrollIntoView(new UiSelector().text(WIFI_SSID).instance(0));')
             'new UiSelector().text("AndroidWifi")')
         el.click()
 
 #перейти в нее
 #чекать каждую секунду в течении 60 секунд и посчитать среднее
-        # self.driver.implicitly_wait(5000)
         driver.implicitly_wait(10)
 
         el = driver.find_element_by_android_uiautomator(
             'new UiSelector().resourceId(\"com.ubnt.usurvey:id/vSiteDetailGaugeSignalStrengthValue\")'
         ).get_attribute("text")
-        print("Signal dBm = " + el)
+        print("First signal dBm = " + el)
 #вывести среднее и умереть смертью храбрых
+        i = 0
+        my_list = []
+        for _ in range(10):
+            el = driver.find_element_by_android_uiautomator(
+                'new UiSelector().resourceId(\"com.ubnt.usurvey:id/vSiteDetailGaugeSignalStrengthValue\")'
+            ).get_attribute("text")
+            el = int(el)
+            my_list.append(el)
+            # print(i)
+            print("Element " + str(i) + " list= " + str(my_list[i]))
+            i += 1
+            time.sleep(1)
+
+        print("среднее значение: ", end="")
+        print(numpy.mean(my_list))
+
 
